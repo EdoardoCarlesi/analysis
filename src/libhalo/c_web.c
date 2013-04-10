21231fb6a8e2d59e5df071b28362abf4089f9781
Comodo <edoardo@comodo.localhost>
2013-04-10 15:23:41 +0200

    minor changes

--- src/libhalo/c_web.c
+++ src/libhalo/c_web.c
@@ -65,6 +65,7 @@ void read_v_web()
 		if(i >= skip) 
 		{	              //1  2  3  4  5  6  7  8  9  10 
 			sscanf(dummy, "%f %f %f %f %f %f %f %f %f %f", 
+					// Read into dummy variables
 				       &a,&a,&a,&a,&a,&a,&a, 
 				       &VWeb[i-1].lambda[0], &VWeb[i-1].lambda[1], &VWeb[i-1].lambda[2]);
 
@@ -105,15 +106,17 @@ void assign_haloes_to_web()
 				iy = (int) GridUnit * Haloes[j].X[1]; 
 				iz = (int) GridUnit * Haloes[j].X[2]; 
 	
-					// Init all node types to zero
+					// Init all node types to minus one
 				for(i=0; i<4; i++)
-					Haloes[j].web_type[i] = 0;
-
+					Haloes[j].web_type[i] = -1;
+					
+					// Find the nearest node's type assuming that
+					// the nodes in the Web file are already ordered
 				index = VWeb[Node(NWeb, ix, iy, iz)].type;
 				Haloes[j].web_type[index] = 1;
 
-				fprintf(stderr, "(%d , %d) type=%d, x=%f, y=%f z=%f; ix=%d iy=%d iz=%d\n",
-			j,Node(NWeb, ix, iy, iz),index,Haloes[j].X[0], Haloes[j].X[1], Haloes[j].X[2], ix, iy, iz);
+			//fprintf(stderr, "(%d , %d) type=%d, x=%f, y=%f z=%f; ix=%d iy=%d iz=%d\n",
+			//j,Node(NWeb, ix, iy, iz),index,Haloes[j].X[0], Haloes[j].X[1], Haloes[j].X[2], ix, iy, iz);
 			}
 		}
 
@@ -132,28 +135,19 @@ int compute_node_type(int i)
 	l2 = VWeb[i].lambda[1];
 	l3 = VWeb[i].lambda[2];
 
-
-	if(l1 > l2 && l2 > l3)
-	{
-	} else {
-		fprintf(stderr, "incorrect ordering l1=%f, l2=%f, l3=%f\n", l1, l2, l3);
-	}
-
-/*
-		if(l1 > l0) j++;
-		if(l2 > l0) j++;
-		if(l3 > l0) j++;
-*/		
+	if(!(l1 > l2 && l2 > l3))
+		fprintf(stderr, 
+	"Problem computing node type, incorrect ordering for the eigenvalues l1=%f, l2=%f, l3=%f\n", 
+			l1, l2, l3);
 
 		if(l3 > l0) j=3;
 		if(l2 > l0 && l3 < l0) j=2;
 		if(l1 > l0 && l2 < l0) j=1;
 		if(l1 < l0) j=0;
-		//else j=0;
 
 	VWeb[i].type = j;
 
-	fprintf(stderr, "%d) NodeType=%d\n", i, j);
+	//fprintf(stderr, "%d) NodeType=%d\n", i, j);
 	
 	return j;
 }
